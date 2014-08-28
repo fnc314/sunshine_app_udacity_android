c185e5d7b726cbf37c5c949f0cb4c0a975c12f9c
Franco N. Colaizzi <FrancoNColaizzi@fnc314.local>
2014-08-28 16:45:09 -0400

    working on implementing settings feature into application

--- app/src/main/java/com/fnc314/sunshine/app/MainActivity.java
+++ app/src/main/java/com/fnc314/sunshine/app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.fnc314.sunshine.app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -53,6 +54,8 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
         return super.onOptionsItemSelected(item);
